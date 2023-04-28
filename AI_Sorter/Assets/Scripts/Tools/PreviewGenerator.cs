be220659a45852cd79a21bf0811835ca7cd8cac5
Tequiloutre <tequiloutre@gmail.com>
2023-04-28 09:26:17 +0200

    Fixed null bytes in PreviewGenerator

--- AI_Sorter/Assets/Scripts/Tools/PreviewGenerator.cs
+++ AI_Sorter/Assets/Scripts/Tools/PreviewGenerator.cs
@@ -20,8 +20,13 @@ public static class PreviewGenerator
 	private static void GenerateObjectSprite(GameObject _object)
 	{
 		string _assetPath = AssetDatabase.GetAssetPath(_object);
+		
+		Texture2D _texture = null;
+		while (_texture == null)
+			_texture = AssetPreview.GetAssetPreview(_object);
+		
+		byte[] _data = _texture.EncodeToPNG();
 		string _localFolder = _assetPath[_assetPath.IndexOf('/')..(_assetPath.LastIndexOf('/') + 1)];
-		byte[] _data = AssetPreview.GetAssetPreview(_object).EncodeToPNG();
 		File.WriteAllBytes(Application.dataPath + _localFolder + _object.name + ".png", _data);
 			
 		AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
